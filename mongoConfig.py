9740eb6b266f62b6605c9a15cbbbc4df20faebb9
LizethJs <126631204+LizethJs@users.noreply.github.com>
2024-10-15 15:24:03 -0500

    Add files via upload

---
+++ mongoConfig.py
@@ -0,0 +1,5 @@
+config = {
+    "URI":"mongodb+srv://jlpenam:Lfl6KmRarbIuOKJ7@cluster0.c05eq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "DATABASE":"SMSStrategy",
+    "COLLECTION":"SMSDelivery"
+}
